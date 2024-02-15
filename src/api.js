313599170c6fa058ceff292bdb723f185d9409f2
Laban Githinji <githinjilaban22@gmail.com>
2024-02-15 06:13:20 +0300

    commot

---
+++ src/api.js
@@ -0,0 +1,11 @@
+ export const geoApioptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': "180c760a99msh33d1adf7863e2bbp176793jsn711e16f9ee45",
+		'X-RapidAPI-Host': "wft-geo-db.p.rapidapi.com",
+	},
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const WEATHER_API_URL ='https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '5d76658ce4b890cc836474e50b76a1a9';
